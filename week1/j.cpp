ab67e9257f46a012fd89218b8ab1e22e1d21172b
KKcuber <kkcubes@gmail.com>
2021-11-11 19:21:30 +0530

    1j

---
+++ week1/j.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define tc    \
+    int t;    \
+    cin >> t; \
+    while (t--)
+
+// We use a greedy strategy here. We always pick the highest number in the set at that moment as the sum of the next 2 numbers.
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    tc{
+        int n; cin >> n;
+        multiset<int> s;
+        for(int i = 0; i<2*n; i++)
+        {
+            int x; cin >> x;
+            s.insert(x);
+        }
+        int yesflag = 0; int sum;
+        queue<int> q;
+        // here i iterates over the possible second value along with the highest element
+        for(int i = 0; i < 2*n-1; i++)
+        {
+            multiset<int> c = s;
+            auto it = c.begin();
+            for(int j = 0; j<i; j++) it = ++it;
+            for(int j = 0; j<n; j++)
+            {
+                auto endit = --c.end();
+                sum = *endit;
+                q.push(*endit); q.push(*it);
+                c.erase(it); c.erase(endit);
+                if(j != n-1)
+                {
+                    it = c.find(sum - *(--c.end()));
+                    if(it == c.end() || it == (--c.end()))
+                    {
+                        while(q.size()) q.pop();
+                        break;
+                    }
+                }
+            }
+            if(c.size() == 0)
+            {
+                yesflag = 1;
+                cout << "YES" << endl;
+                int max = q.front();
+                q.pop();
+                int next = q.front();
+                q.pop();
+                cout << max + next << endl;
+                cout << max << " " << next << endl;
+                for(int k = 1; k < n; k++)
+                {
+                    max = q.front();
+                    q.pop();
+                    next = q.front();
+                    q.pop();
+                    cout << max << " " << next << endl;
+                }
+                break;
+            }
+        }
+        if(!yesflag)
+            cout << "NO" << endl;
+    }
+    return 0;
+}
