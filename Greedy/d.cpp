13a6fa1e0c42c9cd544599d86a538a93152accf3
Kushagra <kkcubes@gmail.com>
2021-11-24 14:04:06 +0530

    2d

--- Greedy/d.cpp
+++ Greedy/d.cpp
@@ -13,14 +13,25 @@ int main()
     int n,k; cin >> n >> k;
     int a[n];
     for(int i=0; i<n; i++) cin >> a[i];
-    set<tuple<ll, int, int>> s;
+    set<tuple<ll, int, int>> s; // (sum, start_index, end_index)
     ll sum = 0;
     for(int i = 0; i<n; i++) sum += a[i];
     cout << sum << " ";
-    s.insert({sum-a[0],0,n-2});
-    s.insert({sum-a[n-1], 1, n-1});
+    s.insert({sum-a[n-1],0,n-2});
+    s.insert({sum-a[0], 1, n-1});
     for(int i = 1; i<k; i++)
     {
-        s.
+        tuple<ll, int, int> to_remove = *(--s.end());
+        s.erase(to_remove);
+        if(get<1>(to_remove) == get<2>(to_remove))
+        {
+            ;
+        }
+        else
+        {
+            s.insert({get<0>(to_remove) - a[get<1>(to_remove)], ++get<1>(to_remove), get<2>(to_remove)});
+            s.insert({get<0>(to_remove) - a[get<2>(to_remove)], get<1>(to_remove), --get<2>(to_remove)});
+        }
+        cout << get<0>(to_remove) << " ";
     }
 }
